3ba0a5d6d0a3eacb4771fc6f03be3b334230b2ce
Raul Oliveira <raulodl@gmail.com>
2024-07-15 13:46:41 -0300

    fix: double scrollbar test 2

--- react/CryptoScript/index.jsx
+++ react/CryptoScript/index.jsx
@@ -7,7 +7,11 @@ const CryptoScript = () => {
       'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js'
     document.head.appendChild(script)
 
-    document.body.style.overflowX = 'initial'
+    document.body.style.overflowY = 'hidden'
+
+    const style = document.createElement('style');
+    style.innerHTML = 'html { overflow: auto; }';
+    document.head.appendChild(style);
   }, [])
 
   return null
