da03ca10ea7690554ac079563adaa4193c93d837
Raul Oliveira <raulodl@gmail.com>
2024-07-18 11:21:03 -0300

    fix: pdp produto cortado

--- react/CryptoScript/index.jsx
+++ react/CryptoScript/index.jsx
@@ -1,17 +1,25 @@
 import { useEffect } from 'react'
+import { useRuntime } from 'vtex.render-runtime'
 
 const CryptoScript = () => {
+  const runtime = useRuntime()
+
+  console.log({runtime})
+
   useEffect(() => {
     const script = document.createElement('script')
     script.src =
       'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js'
     document.head.appendChild(script)
 
-    document.body.style.overflowY = 'hidden'
+    if(runtime.page !== "store.product") {
+      document.body.style.overflowY = 'hidden'
+  
+      const style = document.createElement('style');
+      style.innerHTML = 'html { overflow: auto; }';
+      document.head.appendChild(style);
+    }
 
-    const style = document.createElement('style');
-    style.innerHTML = 'html { overflow: auto; }';
-    document.head.appendChild(style);
   }, [])
 
   return null
