287d601eca7c1fece1ee08162fa93155bfe82986
Raul Oliveira <raulodl@gmail.com>
2024-07-23 17:37:39 -0300

    fix: custom description and availability notify css

--- react/CustomDescription/index.jsx
+++ react/CustomDescription/index.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React from 'react'
 import { useProduct } from 'vtex.product-context'
 
 // import { Container } from './styles';
 
-function CustomDescription() {
-    const productContextValue = useProduct()
+function CustomDescription({ children }) {
+  const productContextValue = useProduct()
+  const hasShortDescription =
+    productContextValue?.product?.properties.find(e => e.name == 'Descrição Objetiva')
+      ?.values.length >= 1
+  const shortDescription = productContextValue?.product?.properties.find(
+    e => e.name == 'Descrição Objetiva'
+  )?.values[0]
 
-    console.log({productContextValue})
-    return (    
-        <div>
-            <div class="Descricao--Inline">
+  return (
+    <div>
+      {hasShortDescription ? (
+        <p
+          style={{
+            color: '#555',
+            marginBottom: 10,
+            fontSize: 12,
+            fontFamily: 'Signika',
+            fontWeight: 400,
+            marginTop: 12,
+            textAlign: 'left',
+          }}
+        >
+          {shortDescription}
+        </p>
+      ) : (
+        [children]
+      )}
+      {/* <div class="Descricao--Inline">
                 <p  dangerouslySetInnerHTML={{ __html: productContextValue.product.metaTagDescription }}></p>
-            </div>
-        </div>
-    );
+            </div> */}
+    </div>
+  )
 }
 
-export { CustomDescription };
+export { CustomDescription }
