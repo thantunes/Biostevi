5d624939f338f7ca3aa5c6bc7f489ba33400867e
Jurazek <jurazek@liveseo.com.br>
2025-02-19 15:38:59 -0300

    feat: create placeholder for text below

---
+++ react/SeoTextPlaceholderBelow/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import styles from './style.module.css'
+
+const SeoTextPlaceholderBelow = props => {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleContent = () => {
+    setIsExpanded(!isExpanded)
+  }
+
+  return (
+    <div>
+      <div
+        className={`${styles.wrapperContentSeo} ${
+          isExpanded ? styles.expanded : ''
+        }`}
+      >
+        <div dangerouslySetInnerHTML={{ __html: props.content }}></div>
+      </div>
+      <button className={styles.btnReadMore} onClick={toggleContent}>
+        {isExpanded ? 'Ler menos' : 'Ler mais'}
+      </button>
+    </div>
+  )
+}
+
+SeoTextPlaceholderBelow.schema = {
+  name: 'seo-text-placeholder-below',
+  title: 'Texto SEO abaixo',
+  description: 'Texto SEO abaixo',
+  type: 'object',
+  properties: {
+    content: {
+      title: 'Conteúdo',
+      type: 'string',
+    },
+  },
+}
+
+export default SeoTextPlaceholderBelow
