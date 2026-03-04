c1e5d167d10eecc583a43d5e2535e5ef221cb9f2
SEOdev <seodev@liveseo.com.br>
2026-03-04 10:58:43 -0300

    13155-fix: Ajuste lógica para ocultar header e footer do template em branco dinamicamente

--- react/CustomReceiveHtml/index.jsx
+++ react/CustomReceiveHtml/index.jsx
@@ -4,11 +4,23 @@ const schema = {
   type: 'object',
   properties: {
     html_content: { title: 'Conteudo em HTML', type: 'string', default: '', widget: { 'ui:widget': 'textarea' } },
+    hideHeaderFooter: {
+      title: 'Ocultar header e footer',
+      description: 'Oculta o header e footer da página (template em branco)',
+      type: 'boolean',
+      default: false,
+    },
   },
-} 
+}
 
-function CustomReceiveHtml({ html_content = '' }) {
-  return <div dangerouslySetInnerHTML={{ __html: html_content }} />
+function CustomReceiveHtml({ html_content = '', hideHeaderFooter = false }) {
+  const content = <div dangerouslySetInnerHTML={{ __html: html_content }} />
+
+  if (hideHeaderFooter) {
+    return <div data-template-type="blank">{content}</div>
+  }
+
+  return content
 }
 
 CustomReceiveHtml.schema = schema
